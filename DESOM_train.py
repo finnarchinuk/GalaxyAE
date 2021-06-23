506f684ce6ca120dbd4f661cc1e1574dded78b49
finnarchinuk <76233047+finnarchinuk@users.noreply.github.com>
2021-06-23 15:06:40 -0700

    Update DESOM_train.py
    Major update to reflect refactoring data type.

--- DESOM_train.py
+++ DESOM_train.py
@@ -2,64 +2,74 @@
 Module for training DESOM
 '''
 
+print('\n\n running DESOM_train')
 #------------------Imports-----------------------
 import argparse
 import numpy as np
-import time
-import sys
-
-from DESOM import DESOM
-import matplotlib.pyplot as plt
-from keras import optimizers
 from astropy.table import Table
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_squared_error
+
+from keras import optimizers
+from DESOM_streamlined import DESOM
 
 #----------------------Arguments----------------------
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--map_size', nargs='+', default=[15, 15], type=int)
-parser.add_argument('--gamma', default=5e-4, type=float, help='coefficient of self-organizing map loss')
-parser.add_argument('--iterations', default=125000, type=int)
-parser.add_argument('--som_iterations', default=100000, type=int)
-parser.add_argument('--batch_size', default=32, type=int)
-parser.add_argument('--Tmax', default=15.0, type=float)
-parser.add_argument('--Tmin', default=0.7, type=float)
-parser.add_argument('--save_path',type=str)
-parser.add_argument('--lr', default=5e-5, type=float)
-parser.add_argument('--latent', default=256, type=int)
-parser.add_argument('--seed', default=1, type=int)
+parser.add_argument('--map_size', nargs='+', default=[8, 8], type=int)
+parser.add_argument('--gamma', default=0.001, type=float, help='coefficient of self-organizing map loss')
+parser.add_argument('--iterations', default=10000, type=int)
+parser.add_argument('--som_iterations', default=10000, type=int)
 
-#Obtain args
+parser.add_argument('--save_epochs', default = 100, type=int)
+parser.add_argument('--model_batch_size', default=256, type=int)
+parser.add_argument('--Tmax', default=10.0, type=float)
+parser.add_argument('--Tmin', default=0.1, type=float)
+parser.add_argument('--save_path',type=str)
+
+parser.add_argument('--lr',type=float)
+parser.add_argument('--latent',type=int)
+parser.add_argument('--seed',type=int)
+
 args = parser.parse_args()
 
 # --------- Log training conditions -------
 print('args')
 print('save_path:',args.save_path)
 print('map size:',args.map_size)
-print('som epochs:',args.som_iterations) #only adjusting SOM weights
-print('both epochs:',args.iterations) #both refers to adjustments of both the SOM and AE components
+print('som epochs:',args.som_iterations)
+print('both epochs:',args.iterations)
 print('gamma:',args.gamma)
 print('T:',args.Tmax,'->',args.Tmin)
 print('lr:',args.lr)
 print('latent:',args.latent)
 print('seed: ',args.seed)
+print('batch_size:',args.model_batch_size)
+my_save_path=args.save_path
 
-#------------------------------------------------ Path names----------------------------------
-X_train_path='X_train_1m.npy'
-Y_train_path='Y_train_1m.npy'
-X_test_path ='X_test_1m.npy'
-Y_test_path ='Y_test_1m.fits'
-print('Using '+ X_train_path+ ' for training.')
+# --------- Path
+SUFFIX = 'june11b'
+x_train_path='x_train1m_'+SUFFIX+'.fits'
+y_train_path='y_train1m_'+SUFFIX+'.fits'
+
+x_test_path ='x_test1m_'+SUFFIX+'.fits'
+y_test_path ='y_test1m_'+SUFFIX+'.fits'
+
+print('using '+ x_train_path+ ' for training')
 
 #----------------------------Load DATA and specify ENCODER DIMENSIONS(ie.input dimensions)-------------------
-X_train=np.load(X_train_path) # These have been Standard Scaled.
-input_dims=X_train.shape[1]
+x_train = Table.read(x_train_path)
 
-print('Apply a MinMaxScaler to training set')
-X_train=MinMaxScaler().fit_transform(X_train.T).T
+input_dims = x_train['raw'][0].shape[0]
 
-print('Train size:',X_train.shape)
+print('applying a StandardScaler to training set along each spectra')
+x_train['raw'] = StandardScaler().fit_transform(x_train['raw'].T).T
+
+print('Train size:',x_train['raw'].shape)
+np.random.seed(args.seed)
+
 np.random.seed(args.seed)
 np.random.shuffle(X_train) # an extra shuffle before training.
 
@@ -68,14 +78,18 @@ np.random.shuffle(X_train) # an extra shuffle before training.
 som = DESOM(input_dims = input_dims, # Data width: 4544
             map_size = args.map_size, # SOM size: (15,15)
             latent = args.latent) # Latent width of AE: 256
-som.initialize()
+#Specify Gamma and Optimizer
 gamma = args.gamma
 optimizer = optimizers.Adam(args.lr)
+#Initialize DESOM
+som.initialize()
+#Compile
 som.compile(gamma, optimizer)
 
+
 #Train DESOM
-som.init_som_weights(X_train)
-som.fit(X_train,
+som.init_som_weights(x_train['raw'])
+som.fit(x_train['raw'],
         iterations = args.iterations,
         som_iterations = args.som_iterations,
         eval_interval=100,
@@ -83,138 +97,143 @@ som.fit(X_train,
         Tmax= args.Tmax,
         Tmin= args.Tmin,
         decay='exponential',
-        save_dir='results/tmp/'+args.save_path+'/')
+        save_dir='results/tmp/'+my_save_path+'/')
 
 #------------------------------------------------- Plot Diagnostics --------------------------------------------------
 map_size=som.map_size
-#--------------------------- Test Set ---------------------
-# Load Test set
-Xmin=np.load(X_test_path) # These have been Standard Scaled.
-print('Applying a MinMaxScaler to Test set.')
-Xmin=MinMaxScaler().fit_transform(Xmin.T).T
-Ymin=Table.read(Y_test_path)
 
-# Run spectra through the model and capture outputs.
-recons,tmp_bmus=som.model.predict(Xmin)
-
-# Bin each spectrum into the best node on the SOM.
-Ymin['bmu']=np.argmin(tmp_bmus,axis=1)
-
-#------------------------- Calculate heightmap and log ML_r at each node --------------------
-attr_av=np.zeros(map_size[0]*map_size[1])
-heightmap=np.zeros(map_size[0]*map_size[1])
-ATTR='ML_r'
-
-for bmu in range(map_size[0]*map_size[1]):
-    masked=(Ymin['bmu']==int(bmu))
-    temp_list=Ymin[ATTR][masked]
-    attr_av[bmu]=np.mean(np.log(temp_list)) # log for ML_r
-    heightmap[bmu]=len(Ymin[masked])
-
-#------------------------ Plot Heightmap and log ML_r at each node --------------------
-plt.figure()
-plt.imshow(attr_av.reshape(map_size))
-plt.title('log ML_r average')
-plt.colorbar()
-plt.savefig('results/tmp/'+args.save_path+'/log_ML_r_average.png')
-plt.clf()
-#
-plt.figure()
-plt.imshow(heightmap.reshape(map_size))
-plt.title('100k input heightmap')
-plt.colorbar()
-plt.savefig('results/tmp/'+args.save_path+'/heightmap.png')
-plt.clf()
-
-#--------------------------- Plot distribution of samples in each node ------------------
-plt.figure()
-plt.xlabel('number of spectra in node')
-plt.ylabel('number of nodes')
-plt.hist(heightmap,bins=20)
-plt.title('heightmap 100k node distribution')
-plt.savefig('results/tmp/'+args.save_path+'/heightmap_distribution.png')
-plt.clf()
-
-#----------------------------- Log reconstruction error for Test Set ---------------------
-print('MSE Test set:',mean_squared_error(recons,Xmin))
-
-#------------------------ Calculate bias in reconstruction error ------------------
-print('delta_mean:',np.mean(recons-Xmin))
-print('delta_median:',np.median(recons-Xmin))
-delta_mean=np.mean(recons-Xmin,axis=1)
-delta_median=np.median(recons-Xmin,axis=1)
-
-#-------------------------- Plot bias in reconstruction error --------------------
-fig,ax=plt.subplots(2,1)
-ax[0].scatter(Ymin['ML_r'],delta_mean,s=4,alpha=0.2)
-ax[0].axhline(0,c='red',alpha=0.5)
-
-ax[1].scatter(Ymin['meansn2'],delta_mean,s=4,alpha=0.2)
-ax[1].axhline(0,c='red',alpha=0.5)
-plt.savefig('results/tmp/'+my_save_path+'/recon_bias.png')
-plt.clf()
-
-#-------------------------- Training set (for comparison) -------------------------
-# Load
-Y_train=Table.read(Y_train_path)
-
-# Run spectra through network and capture outputs
-recons,tmp_bmus=som.model.predict(X_train)
-Y_train['bmu']=np.argmin(tmp_bmus,axis=1)
-
-
-#------------------------ Calculate Heightmap and log ML_r at each node --------------------
-attr_av=np.zeros(map_size[0]*map_size[1])
-heightmap=np.zeros(map_size[0]*map_size[1])
-ATTR='ML_r'
-
-for bmu in range(map_size[0]*map_size[1]):
-    masked=(Y_train['bmu']==int(bmu))
-    temp_list=Y_train[ATTR][masked]
-    attr_av[bmu]=np.mean(np.log(temp_list)) #log for ML_r
-    heightmap[bmu]=len(Y_train[masked])
-
-#------------------------ Plot Heightmap and log ML_r at each node --------------------
-plt.figure()
-plt.imshow(attr_av.reshape(map_size))
-plt.title('log ML_r average')
-plt.colorbar()
-plt.savefig('results/tmp/'+my_save_path+'/log_ML_r_average_training_set.png')
-plt.clf()
-#
-plt.figure()
-plt.imshow(heightmap.reshape(map_size))
-plt.title('900k input heightmap')
-plt.colorbar()
-plt.savefig('results/tmp/'+my_save_path+'/heightmap_training_set.png')
-plt.clf()
-
-#----------------------------- Log reconstruction error for Test Set ---------------------
-print('MSE Train set:',mean_squared_error(recons,X_train))
-
-#------------------------ Calculate bias in reconstruction error ------------------
-print('delta_mean (train):',np.mean(recons-X_train))
-print('delta_median (train):',np.median(recons-X_train))
-delta_mean=np.mean(recons-X_train,axis=1)
-delta_median=np.median(recons-X_train,axis=1)
-
-#-------------------------- Plot bias in reconstruction error --------------------
-fig,ax=plt.subplots(2,1)
-ax[0].scatter(Y_train['ML_r'],delta_mean,s=4,alpha=0.2)
-ax[0].axhline(0,c='red',alpha=0.1)
-
-ax[1].scatter(Y_train['meansn2'],delta_mean,s=4,alpha=0.2)
-ax[1].axhline(0,c='red',alpha=0.1)
-plt.savefig('results/tmp/'+args.save_path+'/recon_bias_training_set.png')
-plt.clf()
-
-#----------------------------- Decoded Prototypes as grid ----------------------------
+# ---------------------- PLOT DECODED PROTOTYPES ----------------
 decoded_prototypes = som.decode(som.prototypes)
-fig, ax = plt.subplots(map_size[0], map_size[1], figsize=(40,40))
+fig, ax = plt.subplots(map_size[0], map_size[1], figsize=(10,10))
 for k in range(map_size[0] * map_size[1]):
     x = decoded_prototypes[k]
     ax[k // map_size[1]][k % map_size[1]].plot(x)
     ax[k // map_size[1]][k % map_size[1]].axis('off')
 plt.subplots_adjust(hspace=1.05, wspace=1.05)
-plt.savefig('results/tmp/'+args.save_path+'/prototypes.png')
+plt.savefig('results/tmp/'+my_save_path+'/prototypes.png')
+
+
+#--------------------------- EVALUATE and PLOT ----------------------------------
+x_test = Table.read(x_test_path) # LOAD TEST SET
+y_test = Table.read(y_test_path)
+
+print('applying a StandardScaler to test set (along spectra)')
+x_test['raw'] = StandardScaler().fit_transform(np.array(x_test['raw']).T).T
+
+
+# ----------------- calculate bmu for test set ----------------
+recons, tmp_bmus = som.model.predict(x_test['raw'])
+tmp_bmus2 = np.argmin(tmp_bmus,axis=1)
+y_test['bmu'] = tmp_bmus2
+
+
+# ----------------------- ML_r, Heightmap, and Distribution -----------------------
+# (ML_r is a measure of mass to light given a spectrum. It's a nice sanity check)
+
+attr_av = np.zeros(map_size[0]*map_size[1])
+heightmap = np.zeros(map_size[0]*map_size[1])
+
+for bmu in range(map_size[0]*map_size[1]):
+    masked = (y_test['bmu'] == int(bmu)) #remove int?
+    temp_list = y_test['ML_r'][masked]
+    attr_av[bmu]=np.median(np.log(temp_list)) #log for ML_r
+    heightmap[bmu]=len(y_test[masked]) #masked.sum() instead?
+
+plt.figure() 
+plt.imshow(attr_av.reshape(map_size))
+plt.title('log ML_r average')
+plt.colorbar()
+plt.savefig('results/tmp/'+my_save_path+'/log_ML_r_median.png')
+plt.clf()
+#
+plt.figure()
+plt.imshow(heightmap.reshape(map_size))
+plt.title('300k input heightmap')
+plt.colorbar()
+plt.savefig('results/tmp/'+my_save_path+'/heightmap.png')
+plt.clf()
+#
+plt.figure()
+plt.xlabel('number of spectra in node')
+plt.ylabel('number of nodes')
+plt.hist(heightmap,bins=20)
+plt.title('heightmap 300k node distribution')
+plt.savefig('results/tmp/'+my_save_path+'/heightmap_distribution.png')
+
+
+# --------------------- Systematic errors in reconstruction ? -------------------
+
+print('test mse:',mean_squared_error(recons, x_test['raw']))
+
+# calculate bias in reconstruction error
+print('delta_mean:',np.mean(recons-x_test['raw']))
+print('delta_median:',np.median(recons-x_test['raw']))
+delta_mean=np.mean(recons-x_test['raw'],axis=1)
+delta_median=np.median(recons-x_test['raw'],axis=1)
+
+plt.clf()
+fig,ax=plt.subplots(2,1)
+ax[0].scatter(y_test['ML_r'],delta_mean,s=4,alpha=0.2)
+ax[0].axhline(0,c='red',alpha=0.5)
+
+ax[1].scatter(y_test['meansn2'],delta_mean,s=4,alpha=0.2)
+ax[1].axhline(0,c='red',alpha=0.5)
+plt.savefig('results/tmp/'+my_save_path+'/recon_bias.png')
+
+
+
+
+
+
+# --------- calculate reconstruction bias and ML_r/heightmap for training set ------
+print('starting plots using train sets\n')
+
+# ------------- calculate reconstruction error --------
+y_train = Table.read(y_train_path)
+
+recons, tmp_bmus = som.model.predict(x_train['raw'])
+tmp_bmus2 = np.argmin(tmp_bmus, axis=1)
+y_train['bmu'] = tmp_bmus2
+
+# -------- plot ML_r and heightmap --------
+attr_av = np.zeros(map_size[0]*map_size[1])
+heightmap = np.zeros(map_size[0]*map_size[1])
+
+for bmu in range(map_size[0]*map_size[1]):
+    masked = (y_train['bmu'] == int(bmu))
+    temp_list = y_train['ML_r'][masked]
+    attr_av[bmu] = np.median(np.log(temp_list)) #log for ML_r
+    heightmap[bmu] = len(y_train[masked]) # masked.sum instaed?
+
+plt.figure()
+plt.imshow(attr_av.reshape(map_size))
+plt.title('log ML_r average')
+plt.colorbar()
+plt.savefig('results/tmp/'+my_save_path+'/log_ML_r_median_training_set.png')
+plt.clf()
+#
+plt.figure()
+plt.imshow(heightmap.reshape(map_size))
+plt.title('800k input heightmap')
+plt.colorbar()
+plt.savefig('results/tmp/'+my_save_path+'/heightmap_training_set.png')
+plt.clf()
+
+
+# ----------- recon bias --------------
+print('train mse:',mean_squared_error(recons,x_train['raw']))
+
+# calculate bias in reconstruction error
+print('delta_mean (train):', np.mean(recons-x_train['raw']))
+print('delta_median (train):', np.median(recons-x_train['raw']))
+delta_mean = np.mean(recons-x_train['raw'],axis=1)
+delta_median = np.median(recons-x_train['raw'],axis=1)
+
+fig,ax=plt.subplots(2,1)
+ax[0].scatter(y_train['ML_r'],delta_mean,s=4,alpha=0.2)
+ax[0].axhline(0,c='red',alpha=0.1)
+
+ax[1].scatter(y_train['meansn2'],delta_mean,s=4,alpha=0.2)
+ax[1].axhline(0,c='red',alpha=0.1)
+plt.savefig('results/tmp/'+my_save_path+'/recon_bias_training_set.png')
 plt.clf()
