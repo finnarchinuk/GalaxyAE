f7c2169dcf353db7d36ecfaa69709709ec94adbd
finnarchinuk <76233047+finnarchinuk@users.noreply.github.com>
2021-07-20 14:31:33 -0700

    Update CNN_1DAE.py
    Tidy up (added some spacings)

--- CNN_1DAE.py
+++ CNN_1DAE.py
@@ -4,38 +4,38 @@ from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, Resha
 from keras.models import Model
 from keras import backend as K
 
-ALPHA=0.1
-def cnn_1dae(input_dims,latent_dims= 64):
+ALPHA = 0.1
+def CNN_1DAE(input_dims, latent_dims= 64):
     #------------------- Encoder--------------
     input_layer = Input(shape=(input_dims,), name='input')
     reshaped_layer = Reshape((input_dims, 1))(input_layer) #Conv1D requires a channel axis
 
-    x = Conv1D(64, 6,strides=1, padding='same')(reshaped_layer) 
-    x = LeakyReLU(alpha=ALPHA)(x)
-    x = MaxPooling1D(pool_size=2)(x)
+    x = Conv1D(64, 6, strides = 1, padding = 'same')(reshaped_layer) 
+    x = LeakyReLU(alpha = ALPHA)(x)
+    x = MaxPooling1D(pool_size = 2)(x)
     
-    x = Conv1D(64, 6,strides=1, padding='same')(x) 
+    x = Conv1D(64, 6, strides = 1, padding = 'same')(x) 
     x = LeakyReLU(alpha=ALPHA)(x)
     x = MaxPooling1D(pool_size=2)(x)
 
-    x = Conv1D(64, 6, strides=1, padding='same')(x) 
-    x = LeakyReLU(alpha=ALPHA)(x)
-    x = MaxPooling1D(pool_size=2)(x) 
+    x = Conv1D(64, 6, strides = 1, padding = 'same')(x) 
+    x = LeakyReLU(alpha = ALPHA)(x)
+    x = MaxPooling1D(pool_size = 2)(x) 
     
-    x = Conv1D(64, 6, strides=1,padding='same')(x) 
-    x = LeakyReLU(alpha=ALPHA)(x)
-    x = MaxPooling1D(pool_size=2)(x) 
+    x = Conv1D(64, 6, strides = 1, padding = 'same')(x) 
+    x = LeakyReLU(alpha = ALPHA)(x)
+    x = MaxPooling1D(pool_size = 2)(x) 
 
     #----------Store dimensions before we flatten.
     shape_before_flattening = K.int_shape(x)[1:]
     #Unpack
     dims, fmaps = shape_before_flattening
-    num_neurons = dims*fmaps 
+    num_neurons = dims * fmaps 
 
     #Wrap up encoder
     x = Flatten()(x)
     x = Dense(latent_dims,)(x)  #Last layer in Encoder
-    encoded = LeakyReLU(alpha=ALPHA,name='z')(x)
+    encoded = LeakyReLU(alpha = ALPHA, name = 'z')(x)
     #Get the latent dimension
     z_dims =  K.int_shape(encoded)[1] #LATENT using K-backend
 
@@ -44,30 +44,30 @@ def cnn_1dae(input_dims,latent_dims= 64):
               
     #------------------ Rest of Autoencoder--------------------
 
-    x = Dense(num_neurons,name='decoder_11')(encoded)
-    x = LeakyReLU(alpha=ALPHA,name='decoder_11a')(x)
-    x = Reshape(shape_before_flattening, name='decoder_10')(x)    #Reshape for Convolutions
+    x = Dense(num_neurons, name = 'decoder_11')(encoded)
+    x = LeakyReLU(alpha = ALPHA, name = 'decoder_11a')(x)
+    x = Reshape(shape_before_flattening, name = 'decoder_10')(x)    #Reshape for convolutional layers
 
     x = UpSampling1D(2, name = 'decoder_9')(x)
-    x = Conv1D(64, 6, padding='same', name = 'decoder_8')(x) 
-    x = LeakyReLU(alpha=ALPHA,name='decoder_8a')(x)
+    x = Conv1D(64, 6, padding = 'same', name = 'decoder_8')(x) 
+    x = LeakyReLU(alpha = ALPHA, name = 'decoder_8a')(x)
     
     x = UpSampling1D(2, name = 'decoder_7')(x) 
-    x = Conv1D(64, 6, padding='same', name = 'decoder_6')(x) 
-    x = LeakyReLU(alpha=ALPHA,name='decoder_6a')(x)
+    x = Conv1D(64, 6, padding = 'same', name = 'decoder_6')(x) 
+    x = LeakyReLU(alpha = ALPHA, name = 'decoder_6a')(x)
     
     x = UpSampling1D(2, name = 'decoder_5')(x) 
-    x = Conv1D(64, 6, padding='same', name = 'decoder_4')(x) 
-    x = LeakyReLU(alpha=ALPHA,name='decoder_4a')(x)
+    x = Conv1D(64, 6, padding = 'same', name = 'decoder_4')(x) 
+    x = LeakyReLU(alpha = ALPHA, name = 'decoder_4a')(x)
     
     x = UpSampling1D(2, name = 'decoder_3')(x) 
-    x = Conv1D(64, 6, padding='same', name = 'decoder_2')(x)
-    x = LeakyReLU(alpha=ALPHA,name='decoder_2a')(x)
+    x = Conv1D(64, 6, padding = 'same', name = 'decoder_2')(x)
+    x = LeakyReLU(alpha = ALPHA, name = 'decoder_2a')(x)
     
-    x = Conv1D(1, 1, activation='linear', padding='same', name='decoder_1')(x)
-    decoded = Flatten(name='decoder_0')(x)
+    x = Conv1D(1, 1, activation = 'linear', padding = 'same', name = 'decoder_1')(x)
+    decoded = Flatten(name = 'decoder_0')(x)
 
-    autoencoder = Model(input_layer, decoded, name='autoencoder') #DEFINE AUTOENCODER
+    autoencoder = Model(input_layer, decoded, name = 'autoencoder') #DEFINE AUTOENCODER
 
     #----------------------- Standalone Decoder------------------
     # Create input for decoder model
@@ -92,7 +92,7 @@ def cnn_1dae(input_dims,latent_dims= 64):
     decoded = autoencoder.get_layer('decoder_1')(decoded)
     decoded = autoencoder.get_layer('decoder_0')(decoded)
 
-    decoder = Model(inputs=encoded_input, outputs=decoded, name='decoder') #DEFINE DECODER
+    decoder = Model(inputs = encoded_input, outputs = decoded, name = 'decoder') #DEFINE DECODER
     print(autoencoder.summary())
     print('AE - LeakyReLU Alpha: {}'.format(ALPHA))
     return (autoencoder, encoder, decoder)
